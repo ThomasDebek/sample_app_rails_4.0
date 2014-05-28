2455908da24f7e82f77651bb974785af01c39741
ThomasDebek <tom.debek@gmail.com>
2014-05-28 22:12:11 +0200

    Dodajmy metode tylko dla uzytkownika

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -8,4 +8,14 @@ module SessionsHelper
   end
 
 
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    remember_token = User.digest(cookies[:remember_token])
+    @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+
 end
