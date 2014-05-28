7afc94e4ca2f097e98ae5f88a079e0959a44de3e
ThomasDebek <tom.debek@gmail.com>
2014-05-28 21:47:04 +0200

    Aby przetestowac zapamietana sesje musimy najpierw zapisac uzytkownika i nastepnie sprawdzic czy atrybut nie jest pusty :)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,12 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should be_valid }
 
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
   describe "when name is not present" do
     before { @user.name = "a " * 51 }
     it { should_not be_valid }
