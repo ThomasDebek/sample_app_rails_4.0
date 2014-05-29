c66e6736bc8836b470df975ce2470f8036810d1d
ThomasDebek <tom.debek@gmail.com>
2014-05-29 14:30:25 +0200

    Uzytkownicy powinni miec prawo do edycji wlasnych informacji. Przedsatwmy to - najpierw zalogujmy sie jako uzytkownik a nastepnie wprowadzmy nasze dzilania. Nie uzywamy tutaj Capybary. A ponadto uzytkownicy ktorzy probuja edytowac profil innego uzytkownika po wykryciu nieatoryzowanego dostepu przekierujemy go do adresu glownego

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,15 +25,34 @@ describe "AuthenticationPages" do
       before { sign_in user }
 
       it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings',    href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
+      it { should have_link('Profile', href: user_path(user)) }
+      it { should have_link('Settings', href: edit_user_path(user)) }
+      it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
     end
   end
 
 
   describe "authorization" do
+
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "submitting a PATCH request to the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -52,7 +71,6 @@ describe "AuthenticationPages" do
       end
     end
   end
-
 
 
 end
