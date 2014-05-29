e63c64782d5c186b8f33baee85aefc56cc8649b7
ThomasDebek <tom.debek@gmail.com>
2014-05-29 19:53:20 +0200

    Dodajmy kolejne testy :)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -120,6 +120,18 @@ describe "UserPages" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit edit_user_path(user) }
 
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
+    end
+
     describe "page" do
       it { should have_content("Update your profile") }
       it { should have_title("Edit user") }
