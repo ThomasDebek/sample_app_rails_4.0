58f82adbd68e2b36554e4f425c4d10739d767899
ThomasDebek <tom.debek@gmail.com>
2014-06-02 21:44:01 +0200

    Revert "Kolejny test dla naszego micropostu :)"
    This reverts commit 1e721504859a899f3182b36a8a7046c478dcb3ad.

--- spec/models/micropost_spec.rb
+++ spec/models/micropost_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe Micropost do
 
   let(:user) { FactoryGirl.create(:user) }
-  before { @micropost = user.microposts.build(content: "Lorem ipsum") }
+  before do
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: user.id)
+  end
 
   subject { @micropost }
 
   it { should respond_to(:content) }
   it { should respond_to(:user_id) }
-  it { should respond_to(:user) }
-  its(:user) { should eq user }
 
   it { should be_valid }
 
