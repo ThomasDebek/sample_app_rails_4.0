552d561275b9dc4a33faa5ac1fe21a7b0b1311a4
ThomasDebek <tom.debek@gmail.com>
2014-06-02 21:43:59 +0200

    Revert "back1"
    This reverts commit c1bbd133eeca5aba86ea3ae156bf70a34ab43015.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,13 +44,6 @@ class UsersController < ApplicationController
     end
   end
 
-
-
-
-
-
-
-
   private
 
   def user_params
@@ -59,12 +52,12 @@ class UsersController < ApplicationController
   end
 
 
- # def signed_in_user
-  #  unless signed_in?
-   #   store_location
-   #   redirect_to signin_url, notice: "Please sign in."
-   # end
-  # end
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
 
   def correct_user
     @user = User.find(params[:id])
