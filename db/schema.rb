ac49cf69dc6f9aac2b121022f3dad6d9267d494a
ThomasDebek <tom.debek@gmail.com>
2014-06-04 12:42:19 +0200

    KONTYNULACJA PO KOMENDZIE REVERT

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20140529165219) do
 
   create_table "users", force: true do |t|
